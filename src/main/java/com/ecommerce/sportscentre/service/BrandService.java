9d4b1d4a28923ad406235c9f3e574d1a774c6751
Arsany11 <11410120220052@stud.cu.edu.eg>
2026-05-19 03:02:40 +0300

    Initial commit

---
+++ src/main/java/com/ecommerce/sportscentre/service/BrandService.java
@@ -0,0 +1,9 @@
+package com.ecommerce.sportscentre.service;
+
+import com.ecommerce.sportscentre.repository.BrandRepository;
+
+import java.util.List;
+
+public interface BrandService {
+    List<BrandRepository>getAllBrands();
+}
